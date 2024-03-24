2563d68f0b6197bbf54451f92cd847dd248c2aab
Jason Mueller <jasonmueller17@yahoo.ca>
2024-03-24 01:10:47 -0400

    about page starting

--- components/about.php
+++ components/about.php
@@ -4,6 +4,7 @@ include('landing/landingHeader.php');
 
 <div class="landing-about-container">
     <h1>GameQuest</h1>
+    <p>The follow are a some frequently asked questions about GameQuest. Please take a look. If you have any further questions please contact us via email.</p>
     <dl>
         <dt>What is GameQuest?</dt>
         <dd>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, earum.</dd>
@@ -13,3 +14,7 @@ include('landing/landingHeader.php');
         <dd>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit reprehenderit quod deleniti porro qui eaque aspernatur hic aperiam perspiciatis dolor.</dd>
     </dl>
 </div>
+
+<?php
+include('landing/landingFooter.php')
+?>

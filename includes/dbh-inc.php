98c4060465467e02bd3af56e457e6594f1eaa21d
Jason Mueller <jasonmueller17@yahoo.ca>
2024-03-28 13:11:05 -0400

    dbh-inc needed definitive port to function on laptop.

--- includes/dbh-inc.php
+++ includes/dbh-inc.php
@@ -5,7 +5,7 @@ $dBUsername = "root";
 $dBPassword = "rootPassword";
 $dBName = "gamequest";
 
-$conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
+$conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName, 3306);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
